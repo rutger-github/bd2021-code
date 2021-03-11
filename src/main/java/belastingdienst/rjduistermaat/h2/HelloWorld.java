794de29cb5e12cda53bad02e2c241865b22ec7ac
Rutger Duistermaat <r.duistermaat@hotmail.com>
2021-03-11 09:07:46 +0100

    [h2] added concept file HelloWorld

---
+++ src/main/java/belastingdienst/rjduistermaat/h2/HelloWorld.java
@@ -0,0 +1,17 @@
+package belastingdienst.rjduistermaat.h2;
+
+import java.util.Scanner;
+
+import static java.lang.Math.abs;
+
+public class HelloWorld {
+
+    /**
+     * @param args CLIarguments
+     */
+    public static void main(String[] args) {
+
+        int plusTwo = abs(-2);
+        System.out.println(plusTwo);
+    }
+}
