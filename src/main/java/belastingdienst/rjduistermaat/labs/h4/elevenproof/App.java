93dae99ea28238185003d4b2d66aa358a82b2771
Rutger Duistermaat <r.duistermaat@hotmail.com>
2021-03-18 09:31:33 +0100

    [labs - h4 - elevenproof] added scanner input and catch exception

--- src/main/java/belastingdienst/rjduistermaat/labs/h4/elevenproof/App.java
+++ src/main/java/belastingdienst/rjduistermaat/labs/h4/elevenproof/App.java
@@ -1,11 +1,24 @@
 package belastingdienst.rjduistermaat.labs.h4.elevenproof;
 
 import belastingdienst.rjduistermaat.labs.h4.elevenproof.core.AccountNumber;
+import belastingdienst.rjduistermaat.labs.h4.elevenproof.core.InvalidAccountNumberException;
+
+import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) {
-        AccountNumber.fromString("73.61.60.221");
-        AccountNumber.fromString("73.62.60.221");
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter an account number");
+        String enteredAccountNumber = in.nextLine();
+
+
+        try {
+            AccountNumber accountNumber1 = AccountNumber.fromString(enteredAccountNumber);
+            System.out.println("Account number " + accountNumber1.getAccountNumber() + " is valid.");
+        } catch (InvalidAccountNumberException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
