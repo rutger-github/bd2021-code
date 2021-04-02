d3880f8cfbf1ee1d4e2de7c2957fc702d9777851
Rutger Duistermaat <r.duistermaat@hotmail.com>
2021-04-02 17:22:05 +0200

    [labs - tdd1] added SoutWrapper

---
+++ src/main/java/belastingdienst/rjduistermaat/labs/tdd1/shared/infrastructure/system/SoutWrapper.java
@@ -0,0 +1,7 @@
+package belastingdienst.rjduistermaat.labs.tdd1.shared.infrastructure.system;
+
+public class SoutWrapper {
+    public static void print(Object value) {
+        System.out.println(value);
+    }
+}
